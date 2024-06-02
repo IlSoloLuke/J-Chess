48006d988df667e15309a366a632d17ceffcb8ef
TheOnlyLuke <135544599+IlSoloLuke@users.noreply.github.com>
2024-06-02 19:26:00 +0200

    initial commit

---
+++ src/main/java/it/theonlyluke/enums/pezzi/SessoPezzi.java
@@ -0,0 +1,12 @@
+package it.theonlyluke.enums.pezzi;
+
+public enum SessoPezzi {
+    M("o"),
+    F("a");
+
+    public final String articolo;
+
+    SessoPezzi(String articolo) {
+        this.articolo = articolo;
+    }
+}
